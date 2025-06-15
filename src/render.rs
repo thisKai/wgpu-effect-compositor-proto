bd59cf76542ba4774da10e4b996071e8308c0eed
Kai <thiskaiguy@gmail.com>
2025-06-15 18:43:29 +0100

    Refactor wallpaper

--- src/render.rs
+++ src/render.rs
@@ -1,14 +1,13 @@
-use wesl::include_wesl;
+use wallpaper::Wallpaper;
+
+mod wallpaper;
 
 pub struct Renderer {
-    wallpaper_pipeline: wgpu::RenderPipeline,
-    wallpaper_texture: WallpaperTexture,
+    wallpaper: Wallpaper,
 }
 impl Renderer {
     pub fn draw(&self, render_pass: &mut wgpu::RenderPass) {
-        render_pass.set_pipeline(&self.wallpaper_pipeline);
-        render_pass.set_bind_group(0, &self.wallpaper_texture.bind_group, &[]);
-        render_pass.draw(0..6, 0..1);
+        self.wallpaper.draw(render_pass);
     }
     pub fn resize(&mut self, queue: &wgpu::Queue, width: u32, height: u32) {}
     pub fn new(
@@ -16,183 +15,8 @@ impl Renderer {
         queue: &wgpu::Queue,
         config: &wgpu::SurfaceConfiguration,
     ) -> Self {
-        let wallpaper_texture = WallpaperTexture::new(device, queue);
+        let wallpaper = Wallpaper::new(device, queue, config);
 
-        let wallpaper_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-            label: Some("wallpaper shader"),
-            source: wgpu::ShaderSource::Wgsl(include_wesl!("wallpaper").into()),
-        });
-        let wallpaper_render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("wallpaper pipeline layout"),
-                bind_group_layouts: &[&wallpaper_texture.bind_group_layout],
-                push_constant_ranges: &[],
-            });
-
-        let wallpaper_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("wallpaper render pipeline"),
-            layout: Some(&wallpaper_render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &wallpaper_shader,
-                entry_point: Some("vs_main"),
-                buffers: &[],
-                compilation_options: wgpu::PipelineCompilationOptions::default(),
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &wallpaper_shader,
-                entry_point: Some("fs_main"),
-                targets: &[Some(wgpu::ColorTargetState {
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::ALPHA_BLENDING),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-                compilation_options: wgpu::PipelineCompilationOptions::default(),
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Cw,
-                cull_mode: None, //Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
-                unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-            multiview: None,
-            cache: None,
-        });
-
-        Self {
-            wallpaper_pipeline,
-            wallpaper_texture,
-        }
-    }
-}
-
-struct WallpaperTexture {
-    bind_group_layout: wgpu::BindGroupLayout,
-    bind_group: wgpu::BindGroup,
-}
-impl WallpaperTexture {
-    fn new(device: &wgpu::Device, queue: &wgpu::Queue) -> Self {
-        let wallpaper_bytes = include_bytes!("905114.png");
-        let wallpaper_image = image::load_from_memory(wallpaper_bytes).unwrap();
-        let wallpaper_rgba = wallpaper_image.to_rgba8();
-
-        use image::GenericImageView;
-        let dimensions = wallpaper_image.dimensions();
-
-        let texture_size = wgpu::Extent3d {
-            width: dimensions.0,
-            height: dimensions.1,
-            // All textures are stored as 3D, we represent our 2D texture
-            // by setting depth to 1.
-            depth_or_array_layers: 1,
-        };
-
-        let wallpaper_texture = device.create_texture(&wgpu::TextureDescriptor {
-            size: texture_size,
-            mip_level_count: 1, // We'll talk about this a little later
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            // Most images are stored using sRGB, so we need to reflect that here.
-            format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            // TEXTURE_BINDING tells wgpu that we want to use this texture in shaders
-            // COPY_DST means that we want to copy data to this texture
-            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-            label: Some("wallpaper texture"),
-            // This is the same as with the SurfaceConfig. It
-            // specifies what texture formats can be used to
-            // create TextureViews for this texture. The base
-            // texture format (Rgba8UnormSrgb in this case) is
-            // always supported. Note that using a different
-            // texture format is not supported on the WebGL2
-            // backend.
-            view_formats: &[],
-        });
-
-        queue.write_texture(
-            // Tells wgpu where to copy the pixel data
-            wgpu::TexelCopyTextureInfo {
-                texture: &wallpaper_texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            },
-            // The actual pixel data
-            &wallpaper_rgba,
-            // The layout of the texture
-            wgpu::TexelCopyBufferLayout {
-                offset: 0,
-                bytes_per_row: Some(4 * dimensions.0),
-                rows_per_image: Some(dimensions.1),
-            },
-            texture_size,
-        );
-
-        let wallpaper_texture_view =
-            wallpaper_texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let wallpaper_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
-            mipmap_filter: wgpu::FilterMode::Nearest,
-            ..Default::default()
-        });
-
-        let texture_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                entries: &[
-                    wgpu::BindGroupLayoutEntry {
-                        binding: 0,
-                        visibility: wgpu::ShaderStages::FRAGMENT,
-                        ty: wgpu::BindingType::Texture {
-                            multisampled: false,
-                            view_dimension: wgpu::TextureViewDimension::D2,
-                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
-                        },
-                        count: None,
-                    },
-                    wgpu::BindGroupLayoutEntry {
-                        binding: 1,
-                        visibility: wgpu::ShaderStages::FRAGMENT,
-                        // This should match the filterable field of the
-                        // corresponding Texture entry above.
-                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
-                        count: None,
-                    },
-                ],
-                label: Some("texture_bind_group_layout"),
-            });
-
-        let wallpaper_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &texture_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&wallpaper_texture_view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::Sampler(&wallpaper_sampler),
-                },
-            ],
-            label: Some("wallpaper_bind_group"),
-        });
-
-        Self {
-            bind_group_layout: texture_bind_group_layout,
-            bind_group: wallpaper_bind_group,
-        }
+        Self { wallpaper }
     }
 }
